64ce39d75d43e270da3cdf7b910481925d5ae6de
Samiha2 <89253466+Samiha2@users.noreply.github.com>
2023-10-21 03:22:34 -0400

    added some styles for sign up page

--- healthifyapp/src/components/SignUp.jsx
+++ healthifyapp/src/components/SignUp.jsx
@@ -5,36 +5,32 @@ export default function SignUp() {
   return (
     <div className='form-modal__container'>
       <div className='form-modal__wrapper'>
+        <img src="" alt="" />
         <div className='sign-up__container'>
           <h2>Sign Up</h2>
           <form className='sign-up__form'>
             <div className='name-inputs'>
               <div className='form-group'>
                 <label>First Name</label>
-                <input type='text' placeholder='John' />
+                <input type='text' placeholder='' />
               </div>
               <div className='form-group'>
                 <label>Last Name</label>
-                <input type='text' placeholder='Doe' />
+                <input type='text' placeholder='' />
               </div>
             </div>
             <div className='form-group'>
-              <label>Username</label>
-              <input type='text' placeholder='johndoe' />
+              <label>Username: </label>
+              <input type='text' placeholder='username' />
             </div>
             <div className='form-group'>
-              <label>Password</label>
+              <label>Password: </label>
               <input type='password' placeholder='Password' />
             </div>
             <button type='submit' className='btn-sign'>
               Let's Go
             </button>
           </form>
-          <div>
-            <p className='have-account'>
-              Have an account? <span>Log In here</span>
-            </p>
-          </div>
         </div>
       </div>
     </div>
