df4eba73e3b22d54c5683784aa642b7e4ef6f296
nartehi <97652878+nartehi@users.noreply.github.com>
2023-10-21 08:58:48 -0400

    fix issue with formatting errors

--- healthifyapp/src/App.jsx
+++ healthifyapp/src/App.jsx
@@ -1,30 +1,15 @@
-<<<<<<< HEAD
-=======
 import React from 'react';
 import Signup from './components/SignUp'; // the path to the Signup component
->>>>>>> 4dfe38f36001780d9a0786c07614a1fa2d9305a0
 import './App.css'
-import HealthDimensions from './components/HealthDimensions'
+import HealthDimensions from './components/HealthDimensions';
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      <div className='home'>
-        <h1> Healthy Kids Zone!</h1>
-        <HealthDimensions />
-      </div> 
-    </>
-  )
-}
-export default App;
-=======
     <div className='App'>
       <h1> Healthy Kids Zone!</h1>
-      <Signup />
+      {/* <Signup /> */}
       <HealthDimensions />
     </div>
   );
 }
 
 export default App;
->>>>>>> 4dfe38f36001780d9a0786c07614a1fa2d9305a0
