8efe2d51a81addd0b532d9c8b2afb1da7338536e
nartehi <97652878+nartehi@users.noreply.github.com>
2023-10-21 02:52:23 -0400

    Homepage elements placed

--- healthifyapp/src/App.jsx
+++ healthifyapp/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import HealthDimensions from './components/HealthDimensions'
 
 function App() {
   return (
     <>
       <div>
         <h1> Healthy Kids Zone!</h1>
+        <HealthDimensions />
       </div>
       
     </>
