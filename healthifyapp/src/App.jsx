261d38409075be658aaa511352734e8c3cc725a9
Alejandro Ramos <ramosa@berea.edu>
2023-10-21 23:28:11 -0400

    book flipping done

--- healthifyapp/src/App.jsx
+++ healthifyapp/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Signup from './components/SignUp'; // the path to the Signup component
 import './App.css'
-import HealthDimensions from './components/HealthDimensions';
+// import HealthDimensions from './components/HealthDimensions';
+// import HealthMap from './components/HealthMap';
+import flipbook from './components/flipbook';
+import HorizontalFlipbook from './components/flipbook';
 function App() {
   return (
     <div className='App'>
       <h1> Healthy Kids Zone!</h1>
       {/* <Signup /> */}
-      <HealthDimensions />
-      <button className='letsgoButn'>Let's Go!</button> {/* Add your button here */}
+      {/* <HealthDimensions />  */}
+      {/* <button className='letsgoButn'>Let's Go!</button> */}
+      {/* <HealthMap /> */}
+      <HorizontalFlipbook />
     </div>
   );
 }
